e18a998a319d10b756c02fb5999df3bfeb4a6770
ubedsalto12 <65477300+ubedsalto12@users.noreply.github.com>
2021-11-13 21:46:14 +0800

    Update kasir.py

--- kasir.py
+++ kasir.py
@@ -1,219 +1,308 @@
+jumlah_pesanan_hari_ini = []
+total_uang_kasir = []
 barang = []
 harga_satuan = [] 
 jumlah_barang = []
 total_harga = []
-pesanan = []
-bank = []
+minuman = [ "Latte     ","Espresso  ","Cappuccino","Americano ","Cold Brew ","Affogato  "]
+harga = [ 15000, 16000, 17000, 18000, 19000, 20000]
 
-minuman = ["Latte     ","Espresso  ","Cappuccino","Americano ","Cold Brew ","Affogato  "]
-
-def daftar_menu():
+def program_menu_awal():
     print("=============================================")
-    print("                    Menu                     ")
-    print("=============================================")
-    print("1. ",minuman[0],"                   Rp 15.000")  
-    print("2. ",minuman[1],"                   Rp 16.000")
-    print("3. ",minuman[2],"                   Rp 17.000")
-    print("4. ",minuman[3],"                   Rp 18.000")
-    print("5. ",minuman[4],"                   Rp 19.000")
-    print("6. ",minuman[5],"                   Rp 20.000")
-    print("=============================================")
-    
-def batalkan_pesanan():
-    print("=============================================")
-    print("1. Simpan Pesanan")
-    print("2. Untuk Batalkan Pesanan")
-    print("=============================================")
-    
-
-def program_kasir():
-    print("=============================================")
-    print("    Selamat Datang Di Program Kasir REZA     ")
+    print("1. Program Admin Password 3 NIM Akhir")
+    print("2. Program Kasir Password 3 NIM Awal")
     print("=============================================")
     while True:
-
-        daftar_menu()
-        pilih = input("Masukkan Pilihan : ")
-        jumlah = int(input("Masukkan Jumlah Barang : "))
-        if pilih == ("1"):
-            barang.append(minuman[0])
-            jumlah_barang.append(jumlah)
-            harga_satuan.append(15000)
-            total_harga.append(jumlah*15000)
-            batalkan_pesanan()
-            pilih1 = input("Masukkan Pilihan : ")
-            if pilih1 == ("2"):
-                barang.remove(minuman[0])
-                jumlah_barang.remove(jumlah)
-                harga_satuan.remove(15000)
-                total_harga.remove(jumlah*15000)
-            elif pilih == ("1"):
-                print("")
-            else:
-                False
-                print("Masukkan Tidak Tersedia Silahkan Input lagi")
-                continue
-    
-        elif pilih == ("2"):
-            barang.append(minuman[1])
-            jumlah_barang.append(jumlah)
-            harga_satuan.append(16000)
-            total_harga.append(jumlah*16000)
-            batalkan_pesanan()
-            pilih1 = input("Masukkan Pilihan : ")
-            if pilih1 == ("2"):
-                barang.remove(minuman[1])
-                jumlah_barang.remove(jumlah)
-                harga_satuan.remove(16000)
-                total_harga.remove(jumlah*16000)
-            elif pilih == ("1"):
-                print("")
-
-        elif pilih == ("3"):
-            barang.append(minuman[2])
-            jumlah_barang.append(jumlah)
-            harga_satuan.append(17000)
-            total_harga.append(jumlah*17000)
-            batalkan_pesanan()
-            pilih1 = input("Masukkan Pilihan : ")
-            if pilih1 == ("2"):
-                barang.remove(minuman[2])
-                jumlah_barang.remove(jumlah)
-                harga_satuan.remove(17000)
-                total_harga.remove(jumlah*17000)
-            elif pilih == ("1"):
-                print("")
-    
-        elif pilih == ("4"):
-            barang.append(minuman[3])
-            jumlah_barang.append(jumlah)
-            harga_satuan.append(18000)
-            total_harga.append(jumlah*18000)
-            batalkan_pesanan()
-            pilih1 = input("Masukkan Pilihan : ")
-            if pilih1 == ("2"):
-                barang.remove(minuman[3])
-                jumlah_barang.remove(jumlah)
-                harga_satuan.remove(18000)
-                total_harga.remove(jumlah*18000)
-            elif pilih == ("1"):
-                print("")
-
-        elif pilih == ("5"):
-            barang.append(minuman[4])
-            jumlah_barang.append(jumlah)
-            harga_satuan.append(19000)
-            total_harga.append(jumlah*19000)
-            batalkan_pesanan()
-            pilih1 = input("Masukkan Pilihan : ")
-            if pilih1 == ("2"):
-                barang.remove(minuman[4])
-                jumlah_barang.remove(jumlah)
-                harga_satuan.remove(19000)
-                total_harga.remove(jumlah*19000)
-            elif pilih == ("1"):
-                print("")
-
-        elif pilih == ("6"):
-            barang.append(minuman[5])
-            jumlah_barang.append(jumlah)
-            harga_satuan.append(20000)
-            total_harga.append(jumlah*20000)
-            batalkan_pesanan()
-            pilih1 = input("Masukkan Pilihan : ")
-            if pilih1 == ("2"):
-                barang.remove(minuman[5])
-                jumlah_barang.remove(jumlah)
-                harga_satuan.remove(20000)
-                total_harga.remove(jumlah*20000)
-            elif pilih == ("1"):
-                print("")
-        else:
-            print("Pilihan Tidak Tersedia Silahkan Input Lagi")
-         
-        print("=============================================")
-        print("1. Untuk Lanjut Berbelanja")
-        print("2. Untuk Berhenti")
-        print("=============================================")
-        lanjut = input("Masukkan Pilihan : ")
-        if lanjut == ("2"):
-            print("")
+        pilih = input("Masukkan Password : ")
+        if pilih == ("084"):
+            program_admin()
             break
-        if lanjut == ("1"):
-            print("")
+        elif pilih == ("210"):
+            program_user()
         else:
-            print("Pilihan Tidak Tersedia Silahkan Input Lagi")     
+            print("Masukkan Tidak Tersedia Silahkan Input lagi")
+
+def program_admin():
+    print("=============================================")
+    print("1. Data Penjualan Hari Ini")
+    print("2. Tambah Menu")
+    print("3. Hapus Menu")
+    print("4. Kembali Ke Menu Awal")
+    print("5. Matikan Program")
+    print("6. Menu")
+    print("=============================================")
+    while True:
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_data_penjualan()
+            break
+        elif pilih == ("2"):
+            program_tambah_menu()
+        elif pilih == ("3"):
+            program_hapus_menu()
+        elif pilih == ("4"):
+            program_menu_awal() 
+        elif pilih == ("5"):
+            print("=============================================")
+            print("           Program Telah Dimatikan           ")
+            print("=============================================")
+            break
+        elif pilih == ("6"):
+            program_tampilan_menu()
+            program_admin()
         
-    print_nota()
-    
-def print_nota():   
+        else:
+            print("Masukkan Tidak Tersedia Silahkan Input Lagi")
+
+def program_user():
+    print("=============================================")
+    print("1. Menu")
+    print("2. Pesan ")
+    print("3. Hapus pesanan")
+    print("4. Ubah Jumlah Pesanan")
+    print("5. Bayar Pesanan")
+    print("6. Menu Awal")
+    print("=============================================")
+    while True:
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_tampilan_menu()
+            program_user()
+        elif pilih == ("2"):
+            program_kasir()
+        elif pilih == ("3"):
+            program_hapus_pesanan()
+        elif pilih == ("4"):
+            program_ubah_pesanan()
+        elif pilih == ("5"):
+            program_print_nota()
+        elif pilih == ("6"):
+            program_menu_awal()
+
+def program_ubah_pesanan():
     print("=========================================================================================")
     print("                                          Nota                                           ")
     print("=========================================================================================")
-    print("No.     Nama Barang                     Harga satuan         Jumlah             Total    ")
+    print("No.     Nama Barang                     Harga satuan         Jumlah")
     for x in range(0,len(barang)):
-		    print("{}        {}                        Rp{}               {}             Rp{} ".format(x+1, barang[x], harga_satuan[x], jumlah_barang[x],total_harga[x]))
-
+		    print("{}        {}                        Rp{}               {} ".format(x+1, barang[x], harga_satuan[x], jumlah_barang[x],))
     print("=========================================================================================")
-    print("Total Harga : RP ", sum(total_harga))
+    print(barang)
+    print(harga_satuan)
+    print(jumlah_barang)
+    print(total_harga)
+    del (total_harga[-1])
+    print(total_harga)
 
+        
+
+def program_milih_ubah():
+    print("1. Ubah Jumlah Lagi")
+    print("2. Kembali Ke Menu")
+    pilih = input("Masukkan Pilihan : ")
+    while True:
+        if pilih == ("1"):
+            program_ubah_pesanan()
+        elif pilih == ("2"):
+            program_user()
+
+def program_hapus_pesanan():
+    print("=========================================================================================")
+    print("                                          Nota                                           ")
+    print("=========================================================================================")
+    print("No.     Nama Barang                     Harga satuan         Jumlah")
+    for x in range(0,len(barang)):
+		    print("{}        {}                        Rp{}               {} ".format(x+1, barang[x], harga_satuan[x], jumlah_barang[x],))
+    print("=========================================================================================")
+    while True: 
+        hapus = int(input("Masukkan Nomor Menu Yang Ingin Di Hapus (Ketik 0 Untuk Kembali Ke Menu User) : "))
+        hapus -= 1
+        if hapus != (-1):
+            del barang[hapus]
+            del harga_satuan[hapus]
+            del jumlah_barang[hapus]
+            program_hapus_pesanan()
+        else:
+            program_user()
+        
+
+
+def program_tambah_menu():
+    tambah = input("Masukkan Nama Menu Yang Ingin Ditambah : ")
+    tambah_harga = int(input("Masukkan Harga"))
+    minuman.append(tambah)
+    harga.append(tambah_harga)
+    print("=============================================")
+    print("1. Tambah Menu Lagi")
+    print("2. Kembali Ke program Admin")
+    print("=============================================")
+    while True: 
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_tambah_menu()
+        elif pilih == ("2"):
+            program_admin()
+        else:
+            print("Masukkan Tidak Tersedia Silahkan Input Lagi")
+
+def program_hapus_menu():
+    program_tampilan_menu()
+    hapus = int(input("Masukkan Nomor Menu Yang Ingin Di Hapus (Ketik 0 Untuk Kembali Menu User) : "))
+    hapus -= 1
+    del minuman[hapus]
+    del harga[hapus]
+    print("=============================================")
+    print("1. Hapus Menu Lagi")
+    print("2. Kembali Ke program Admin")
+    print("=============================================")  
+    while True: 
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_hapus_menu()
+        elif pilih == ("2"):
+            program_admin()
+        else:
+            print("Masukkan Tidak Tersedia Silahkan Input Lagi")
+
+
+def program_data_penjualan():
+    print(f"Pesanan Hari Ini : {sum(jumlah_pesanan_hari_ini)}\nTotal Uang Yang Ada Dikasir : {sum(total_uang_kasir)}")
+    print("=============================================")
+    print("1. Kembali Ke Menu Awal")
+    print("2. Matikan Program")
+    print("=============================================")
+    while True:
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_menu_awal()
+        elif pilih == ("2"):
+            print("=============================================")
+            print("           Program Telah Dimatikan           ")
+            print("=============================================")
+            break
+        else:
+            print("Masukkan Tidak Tersedia Silahkan Input Lagi")
+
+def program_kasir():
+    program_tampilan_menu()
+    while True:
+        pilih = int(input("Masukkan Pilihan : "))
+        jumlah = int(input("Masukkan Jumlah Barang : "))
+        pilih -= 1
+        if pilih == pilih:
+            barang.append(minuman[pilih])
+            jumlah_barang.append(jumlah)
+            harga_satuan.append(harga[pilih])
+            total_harga.append(jumlah*harga_satuan)
+            program_simpan_batalkan_pesanan()
+
+        else: 
+            print("Masukkan Tidak Tersedia Silahkan Input lagi")
+
+def program_tampilan_menu():
+    print(" ========= Menu Coffe Shop APD3 =========")
+    for i in range(0, len(minuman)):
+        print(f"        {i+1}. {minuman[i]}  Rp{harga[i]}")
+
+def program_simpan_batalkan_pesanan():
+    print("=============================================")
+    print("1. Simpan Pesanan")
+    print("2. Batalkan Pesanan")
+    print("=============================================")
+    while True:
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_menu_pesan_lagi()
+        elif pilih == ("2"):
+            program_batalkan_pesanan()
+        else:
+            print("Masukkan Tidak Tersedia Silahkan Input Lagi")
+
+def program_batalkan_pesanan():
+    del barang[-1]
+    del jumlah_barang[-1]
+    del total_harga[-1]
+    del harga_satuan[-1]
+    program_menu_pesan_lagi()
+
+def program_menu_pesan_lagi():
+    print("=============================================")
+    print("1. Pesan Lagi")
+    print("2. Selesai (print Nota)")
+    print("3. Menu User")
+    print("=============================================")
+    while True:
+        pilih = input("Masukkan Pilihan : ")
+        if pilih == ("1"):
+            program_kasir()
+        elif pilih == ("2"):
+            program_print_nota()
+        elif pilih == ("3"):
+            program_user()
+        else:
+            print("Masukkan Tidak Tersedia Silahkan Input Lagi")
+
+def program_print_nota():
+    print("=========================================================================================")
+    print("                                          Nota                                           ")
+    print("=========================================================================================")
+    print("No.     Nama Barang                     Harga satuan         Jumlah")
+    for x in range(0,len(barang)):
+		    print("{}        {}                        Rp{}               {} ".format(x+1, barang[x], harga_satuan[x], jumlah_barang[x],))
+ 
+    print("=========================================================================================")
+    print("Total Harga : RP ", sum(total_harga[x]))
+    uang_dibayar = sum(total_harga[x])
+    
     bayar = int(input("Masukkan Uang Pembayaran : "))
-    print("Total Harga : RP ",sum(total_harga))
+    if bayar >= uang_dibayar:
+        print("Terima Kasih Telah Berbelanja")
+    else:
+        print("Uang Anda Tidak Cukup")
+        program_print_nota()
+            
+        
+    print("Total Harga : RP ",sum(total_harga[x]))
     print("Jumlah uang : RP ", bayar)
-    print("Kembalian : RP ", (bayar-sum(total_harga)))
+    print("Kembalian : RP ", (bayar-sum(total_harga[x])))
     print("")
     print("=============================================")
-    pesanan.append(1)
-    sum(pesanan)
-    bank.append(sum(total_harga))
+    jumlah_pesanan_hari_ini.append(1)
+    total_uang_kasir.append(sum(total_harga[x]))
     
-    ulang_program()
-    
-    
+    a = 0
+    while a <= len(barang):
+        for y in barang[:]:
+            y == barang[0]
+            barang.remove(y)
+            a+=1
+    b = 0           
+    while b <= len(harga_satuan):
+        for y in harga_satuan[:]:
+            y == harga_satuan[0]
+            harga_satuan.remove(y)
+            b+=1
+    c = 0
+    while c <= len(jumlah_barang):
+        for y in jumlah_barang[:]:
+            y == jumlah_barang[0]
+            jumlah_barang.remove(y)
+            c+=1
+    d = 0
+    while d <= len(total_harga):
+        for y in total_harga[:]:
+            y == total_harga[0]
+            total_harga.remove(y)
+            d+=1
 
-def ulang_program():    
-    print("=============================================")
-    print("1. Program Kasir")
-    print("2. Menu Utama")
-    print("3. Matikan Program")
-    print("=============================================")
-    pilih3 =input("Masukkan Pilihan: ")
-    while True: 
-        if pilih3 == ("1"):
-            program_kasir()
-            break
-        elif pilih3 == ("2"):
-            menu_utama()
-        elif pilih3 == ("3"):
-            break
+    program_user()
+        
 
-def menu_utama():
-    username = input("Masukkan Username : ")
-    print("=============================================")
-    print("Masukkan Password 3 NIM Terakhir Untuk Masuk Ke  Program Admin")
-    print("Masukkan Password 3 NIM Awal Untuk Masuk Ke program Kasir (User)")
-    print("=============================================")
-    password =input("Masukkan Password: ")
-    while True:
-        if password == ("084"):
-            print(f"Selamat Datang {username} Anda Masuk Ke Program Admin")
-            program_admin()
-        elif password == ("210"):
-            print(f"Selamat Datang {username} Anda Masuk Ke Program Kasir (User)")
-            program_kasir()
-        else:
-            print("Password Salah")
-            return menu_utama()
-def program_admin():
-    print("=============================================")
-    print("1. Keuntungan  Harian")
-    print("2. Kembali Ke Menu Utama")
-    pilih = input("Masukkan Pilihan : ")
-    if pilih == ("1"):
-        print(f"Pesanan Hari Ini{pesanan}\nDuit Di Kasir{bank}")
-        program_admin()
-    elif pilih == ("2"):
-        menu_utama()
 
-menu_utama()
 
+#PROGRAM UTAMA
+print("=============================================")
+print("    Selamat Datang Di Program Kasir REZA     ")
+print("=============================================")
+program_menu_awal()
+            
